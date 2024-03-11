a83bca16537e8ac7612877f301c9b6776594aeba
bedirhan <bedirhansaricayir@gmail.com>
2024-03-11 23:33:25 +0300

    created first version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.reelsjoke.app"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.0"
+        versionCode = 4
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
